44f5907edc69080238817d4085809ef507eb7efd
moipo <moipochtoviyyashik@inbox.ru>
2022-11-17 17:07:06 +0300

    add frontend

--- shop/urls.py
+++ shop/urls.py
@@ -3,10 +3,17 @@ from .views import *
 from .api import api_change_cart
 
 urlpatterns = [
-    path('', Prototype.index),
-    path('cart', Prototype.cart, name = "cart"),
-    path('product/<slug:product_slug>', Prototype.product, name = "product" ),
-    # path('ProductApi/change_cart', ProductApi.change_cart, name = "change_cart" ),
+    path('prototype', Prototype.index),
+    path('prototype/cart', Prototype.cart, name = "prototype_cart"),
+    path('prototype/product/<slug:product_slug>', Prototype.product, name = "prototype_product" ),
+    # path('prototype/ProductApi/change_cart', ProductApi.change_cart, name = "change_cart" ),
+
     path('api/change_cart', api_change_cart, name = "api_change_cart"),
-    path('shop/index', Shop.index, name = "shop_index")
+    path('cart', Shop.cart, name = "cart"),
+    path('checkout', Shop.checkout, name = "checkout"),
+    path('contact', Shop.contact, name = "contact"),
+    path('detail', Shop.detail, name = "detail"),
+    path('index', Shop.index, name = "index"),
+    path('shop', Shop.shop, name = "shop")
+
 ]
