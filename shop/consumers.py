d55ad960d5e113a6296b9d4fb97b5c28314caacf
moipo <moipochtoviyyashik@inbox.ru>
2022-12-10 13:31:35 +0300

    add async websocket

--- shop/consumers.py
+++ shop/consumers.py
@@ -8,8 +8,6 @@ import json
 
 class ExchangeRateConsumer(AsyncConsumer):
     async def websocket_connect(self,event):
-        print("it worked")
-        # await self.accept()
         a = 63.15
 
         try:
@@ -33,99 +31,3 @@ class ExchangeRateConsumer(AsyncConsumer):
             'type':'websocket.send',
             "text" : data,
             })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import json
-# from channels.generic.websocket import WebsocketConsumer
-# from time import sleep
-# from random import randint
-# import requests
-# import json
-#
-# class ExchangeRateConsumer(WebsocketConsumer):
-#     def connect(self):
-#         print("it worked")
-#         self.accept()
-#         a = 63.15
-#
-#         try:
-#             latest_link = "https://openexchangerates.org/api/latest.json"
-#             app_id = "?app_id=230c6dfde9e04b19a187f83b726fdbc9"
-#             response = requests.get(latest_link + app_id)
-#             data = json.loads(response.text)
-#             a = round(float(data["rates"]["RUB"]),2)
-#         except:
-#             a = 63.15
-#
-#
-#         while True:
-#             sleep(1.8)
-#             data = str(float('%.2f' % float(a+randint(-15, 15)/100)))
-#             if data[-2] == ".": data += "0"
-#
-#             self.send(text_data = json.dumps({
-#             "data" : data,
-#             }))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # def receive(self, text_data):
-    #     print(text_data)
-    #     a = 63.15
-    #
-    #     try:
-    #         latest_link = "https://openexchangerates.org/api/latest.json"
-    #         app_id = "?app_id=230c6dfde9e04b19a187f83b726fdbc9"
-    #         response = requests.get(latest_link + app_id)
-    #         data = json.loads(response.text)
-    #         a = round(float(data["rates"]["RUB"]),2)
-    #     except:
-    #         a = 63.15
-    #
-    #
-    #     while True:
-    #         sleep(1.8)
-    #         data = str(float('%.2f' % float(a+randint(-15, 15)/100)))
-    #         if data[-2] == ".": data += "0"
-    #
-    #         self.send(text_data = json.dumps({
-    #         "data" : data,
-    #         }))
