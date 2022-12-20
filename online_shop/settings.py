5b322a738e7a74a01a6df41482a63154b302fbd0
moipo <moipochtoviyyashik@inbox.ru>
2022-12-20 21:46:44 +0300

    add api restrictions

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -79,23 +79,23 @@ ASGI_APPLICATION = 'online_shop.asgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': "db",
-        'PASSWORD': 'a123',
-        'PORT': 5432
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': "db",
+#         'PASSWORD': 'a123',
+#         'PORT': 5432
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
--- shop/consumers.py
+++ shop/consumers.py
@@ -12,8 +12,39 @@ class ExchangeRateConsumer(AsyncConsumer):
 
         #This free API gives only 1000 requests/month
         #This is why the imitation of frequent API calls is used here.
-        a = 63.15
 
+
+
+
+        try:
+            with open("last_websocket_call_time.pickle", "rb") as f:
+                last_api_call = pickle.load(f)
+            time_diff = int((datetime.now()-last_api_call).total_seconds())
+            print(time_diff)
+            if time_diff > 10*60:
+                with open("last_websocket_call_time.pickle", "wb") as f:
+                    pickle.dump(datetime.now(), f)
+                    # api call
+            else:
+                #no api call
+
+
+        except:
+            with open("last_websocket_call_time.pickle", "wb") as f:
+                pickle.dump(datetime.now(), f)
+
+
+
+
+
+
+
+
+
+
+
+
+        a = 63.15
         try:
             latest_link = "https://openexchangerates.org/api/latest.json"
             app_id = "?app_id=230c6dfde9e04b19a187f83b726fdbc9"
@@ -23,11 +54,17 @@ class ExchangeRateConsumer(AsyncConsumer):
         except:
             a = 63.15
 
+
+
+
+
+
+
         await self.send({
         'type':'websocket.accept'})
 
         while True:
-            await asyncio.sleep(1.8)
+            await asyncio.sleep(2.2)
             data = str(float('%.2f' % float(a+randint(-15, 15)/100)))
             if data[-2] == ".": data += "0"
 
